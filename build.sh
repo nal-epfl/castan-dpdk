62c0770d6735effe069f581431bd55fd4fa1c828
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2018-04-24 16:30:41 +0200

    Adapted build config.

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 make config T=x86_64-native-linuxapp-gcc
-make -kj $(grep -c 'processor' /proc/cpuinfo)
+
+sed -i -e 's/CONFIG_RTE_LIBRTE_PMD_PCAP=n/CONFIG_RTE_LIBRTE_PMD_PCAP=y/' build/.config
+
+EXTRA_CFLAGS='-O2 -g' make -kj $(nproc)
